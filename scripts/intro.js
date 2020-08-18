beb87898aa1944b1ba4df4889210adf6ba2484f0
Bruna Mikaelly <brunamikaelly8824@outlook.com>
2020-08-17 22:59:47 -0300

    bug fix

---
+++ scripts/intro.js
@@ -0,0 +1,62 @@
+//coments
+//document.write("Hello World")
+
+//variaveis, tipos de dados
+// let myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+//string
+// const s1 = "Isso é uma string"
+// const s2 = 'Isso também é um string'
+// const s3 = `Isso é uma string também`
+
+
+//number
+// const n1 = 1 
+// const n2 = 12
+// document.write(n1+n2)
+
+//boolean - true ou false
+// const Btrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+//objeto possuem propriedade e funcionalidades
+// const pessoa = {
+//   altura: "1,80",
+// idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande logica de correr")
+//   }
+// }
+// pessoa.correr()
+
+//Array - vetores
+//[ ... ]
+//  const colecaoDeBolinhas = ["blue", "green", 1, {name:"My Name"}]
+
+//  document.write(colecaoDeBolinhas[3].name)
+
+//funções - funcionalidades - atalhos - reuso de código
+// function SayMyName(name){
+//   document.write(name)
+// }
+// //executar
+// SayMyName("Douglas")
+// SayMyName("lilith")
+
+//condicionais
+// const notaFinal = 7
+// if(notaFinal < 5 ) {
+//   document.write("Reprovado") 
+// } else {
+//  document.write("Aprovado")
+// }
+
+//loop repetições
+// for (i = 0; i < 10; i++){
+//   document.write(`<p>${i}</p>`)
+// }
